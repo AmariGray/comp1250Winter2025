d351ab070b49bc7e35b18aedf11075c52d6ef6dc
ProfBlanc <34591333+ProfBlanc@users.noreply.github.com>
2025-01-29 09:56:55 -0500

    Create f3.py

---
+++ wk4/f3.py
@@ -0,0 +1,14 @@
+
+
+values = ["hello", 123, True, 98.76, ["a", "b", "c"]]
+
+print(values[4])  # ['a', 'b', 'c']
+print(values[-3]) # True
+len(values)  # number of elements in list
+
+print(values[len(values) * -1])
+
+print(values[len(values) * -1][2])  # 'l'   ???
+print(values[-1][-2])
+
+
