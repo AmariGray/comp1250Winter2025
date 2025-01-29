d4782497c7681746c6962a6171ad32462d8db3df
ProfBlanc <34591333+ProfBlanc@users.noreply.github.com>
2025-01-29 09:56:18 -0500

    Create f1.py

---
+++ wk4/f1.py
@@ -0,0 +1,23 @@
+
+"""
+Lists and Tuples
+    collection data types
+        one variable/identifier that stores many values
+
+courses_codes
+    comp1250
+    comp1176
+    comp3044
+    math1172
+    comp3105
+    
+"""
+
+course_code = ["comp1250", "comp1176", "comp3044", "comp1172", "comp3105"]
+
+print(course_code[-2])
+
+# words are a series/collection of letter/characters
+
+name = "Ben"
+print(name[0], name[1], name[2])
